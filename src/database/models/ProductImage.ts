a4d150408f1471fb15bbf85b08fae9e4f7d23c06
Pedrosavioo <pedrosaviolima618@gmail.com>
2024-11-28 13:11:56 -0300

    feat: product images model

---
+++ src/database/models/ProductImage.ts
@@ -0,0 +1,58 @@
+import { Model, DataTypes, Sequelize } from "sequelize";
+import db from ".";
+
+class ProductImage extends Model {
+   declare id: number;
+   declare productId: number;
+   declare enabled: boolean;
+   declare path: string;
+   declare createdAt: Date;
+   declare updatedAt: Date;
+}
+
+ProductImage.init(
+   {
+      id: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         autoIncrement: true,
+         primaryKey: true,
+      },
+      productId: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         references: {
+            model: "product",
+            key: "id",
+         },
+         onDelete: "CASCADE",
+         onUpdate: "CASCADE",
+      },
+      enabled: {
+         type: DataTypes.BOOLEAN,
+         allowNull: true,
+         defaultValue: false,
+      },
+      path: {
+         type: DataTypes.STRING,
+         allowNull: false,
+      },
+      created_at: {
+         type: DataTypes.DATE,
+         allowNull: false,
+         defaultValue: Sequelize.fn("NOW"),
+      },
+      updated_at: {
+         type: DataTypes.DATE,
+         allowNull: false,
+         defaultValue: Sequelize.fn("NOW"),
+      },
+   },
+   {
+      sequelize: db,
+      tableName: "product_images",
+      underscored: true,
+   }
+);
+
+export default ProductImage;

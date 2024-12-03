1ff606e5dc54b810e487eb625843331bb25e531a
Pedrosavioo <pedrosaviolima618@gmail.com>
2024-12-03 16:02:43 -0300

    feat: auth middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+import JwtTokenService from "../services/jwt.service";
+import User from "../database/models/User";
+import { NotFoundException } from "../utils/exception";
+import IUser from "../interfaces/interface";
+
+declare global {
+   namespace Express {
+      interface Request {
+         authUser: IUser
+      }
+   }
+}
+
+export default async function authMiddleware(
+   req: Request,
+   res: Response,
+   next: NextFunction
+) {
+   try {
+      const token = req.cookies["token"];
+
+      if (token) {
+         const tokenVerify = await JwtTokenService.verify(token);
+         const user = await User.findOne({
+            where: {
+               email: tokenVerify,
+            },
+         });
+
+         if (!user) {
+            throw new NotFoundException("User not found!");
+         }
+
+         if (user) {
+            req.authUser = user;
+         }
+
+         next();
+      }
+
+      if (!token) {
+         console.log('token nao informado');
+      }
+   } catch (error) {
+      next(error);
+   }
+}
